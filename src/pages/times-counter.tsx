6b484e71d84beaf5e76f8ed306e25009e612105d
DominikPrusa <prusa.do@gmail.com>
2026-03-09 08:22:05 +0100

    moving functions

--- src/pages/times-counter.tsx
+++ src/pages/times-counter.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
-import { formatSecondsToMMSS } from "../utils/TimeConversions";
+import {
+  formatSecondsToMMSS,
+  formatToHHMMSS,
+  timeToSeconds,
+} from "../utils/TimeConversions";
 
 const rows = Array.from({ length: 8 }, (_, i) => i + 1);
 
@@ -97,35 +101,6 @@ const TimesCounter = () => {
   }, [formData.stations]);
 
   useEffect(() => {
-    const timeToSeconds = (time: string) => {
-      if (!time) return 0;
-
-      const parts = time.split(":").map(Number);
-
-      if (parts.length === 3) {
-        const [hours, minutes, seconds] = parts;
-        return hours * 3600 + minutes * 60 + seconds;
-      }
-
-      if (parts.length === 2) {
-        const [hours, minutes] = parts;
-        return hours * 3600 + minutes * 60;
-      }
-
-      return 0;
-    };
-
-    const formatToHHMMSS = (totalSeconds: number) => {
-      const safe = Math.max(0, totalSeconds);
-      const hours = Math.floor(safe / 3600);
-      const minutes = Math.floor((safe % 3600) / 60);
-      const seconds = safe % 60;
-
-      return [hours, minutes, seconds]
-        .map((value) => String(value).padStart(2, "0"))
-        .join(":");
-    };
-
     if (formData.startTime && formData.endTime) {
       const startSeconds = timeToSeconds(formData.startTime);
       const endSeconds = timeToSeconds(formData.endTime);
--- src/utils/TimeConversions.ts
+++ src/utils/TimeConversions.ts
@@ -1,6 +1,38 @@
+//363 -> 06:03
 export const formatSecondsToMMSS = (totalSeconds: number) => {
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
 
   return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+};
+
+// 1:01:02 -> 3662
+export const timeToSeconds = (time: string) => {
+  if (!time) return 0;
+
+  const parts = time.split(":").map(Number);
+
+  if (parts.length === 3) {
+    const [hours, minutes, seconds] = parts;
+    return hours * 3600 + minutes * 60 + seconds;
+  }
+
+  if (parts.length === 2) {
+    const [hours, minutes] = parts;
+    return hours * 3600 + minutes * 60;
+  }
+
+  return 0;
+};
+
+//3662 -> 1:01:02
+export const formatToHHMMSS = (totalSeconds: number) => {
+  const safe = Math.max(0, totalSeconds);
+  const hours = Math.floor(safe / 3600);
+  const minutes = Math.floor((safe % 3600) / 60);
+  const seconds = safe % 60;
+
+  return [hours, minutes, seconds]
+    .map((value) => String(value).padStart(2, "0"))
+    .join(":");
 };
